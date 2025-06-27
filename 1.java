406b0bfc9c53f2a877597d03629516aa50070d04
Chek1007 <9211783+Chek1007@users.noreply.github.com>
2025-06-27 10:40:06 +0800

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,8 @@
+test111.corp.kuaishou.com
+user.password = 1111
+token=22222
+secret = 333333
+
+  https://npm.corp.kuaishou.com/@google/generative-ai/-/generative-ai-0.24.1.tgz
+
+ http://kim-robot.kwaitalk.com/
